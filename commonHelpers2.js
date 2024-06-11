363be312b216017b667b245856f9499cd433c962
ihor-mikheyev <69951048+ihor-mikheyev@users.noreply.github.com>
2024-06-11 16:59:17 +0000

    Deploying to gh-pages from @ ihor-mikheyev/goit-js-hw-09@6d430572d8a5741ad29d2883f24b57ee15e72507 🚀

---
+++ commonHelpers2.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-3cfb730f.js";const e={email:"",message:""},t=document.querySelector(".feedback-form"),s="feedback-form-state",o=JSON.parse(localStorage.getItem(s));o&&(Object.assign(e,o),t.email.value=e.email,t.message.value=e.message);t.addEventListener("input",a=>{e[a.target.name]=a.target.value,localStorage.setItem(s,JSON.stringify(e))});t.addEventListener("submit",a=>{a.preventDefault(),localStorage.removeItem(s),t.reset()});
+//# sourceMappingURL=commonHelpers2.js.map
